9fd85d6bfec09d57b78997994d90147b525a6c49
Daylin Henry <daylinhenry@Daylins-MacBook-Pro.local>
2017-11-24 14:43:51 -0600

    first commit

---
+++ src/main/java/com/example/testproj/TestprojApplication.java
@@ -0,0 +1,12 @@
+package com.example.testproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestprojApplication.class, args);
+	}
+}
